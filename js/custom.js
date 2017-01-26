940af7186c48fd0f1c73d96898074c3b23517bfa
sahadeva <acounts@sahadeva.com>
2017-01-26 11:11:09 -0800

    add files

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+  $('#img-zoom').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('#modal-img').attr('src',recipient)
+  });
+
+});
